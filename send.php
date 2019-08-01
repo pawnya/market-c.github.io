e7096afc1672774c3c67bcb118c642dae807e6a8
Павел Иванов <pavelivanov@Mac-Pro-pro100-Anton.local>
2019-08-01 14:27:52 +0400

    form send

--- send.php
+++ send.php
@@ -1,20 +1,18 @@
 <?php
+    if($_POST) {
+        var_dump($POST);
+        die();
+        $to = "p.ivanov@pro100agency.ru"; //куда отправлять письмо
+        $from = "no-replay@mail.com"; // от кого
+        $subject = $_POST['theme']; //тема
+        $message = "Имя: ".$_POST['name']."\r\n";
+        $message .= "Телефон: ".$_POST['phone']."\r\n";
+        $message .= "Email: ".$_POST['Email'];
+        $headers = "From: $from\r\nReplay-To: $from\r\nContent-type: text/plain; charset=utf-8\r\n";
+        $result = mail($to, $subject, $message, $headers);
 
-    print_r($_POST['name']);
-    // if($_POST) {
-    //     var_dump($POST);
-    //     die();
-    //     $to = "p.ivanov@pro100agency.ru"; //куда отправлять письмо
-    //     $from = "no-replay@mail.com"; // от кого
-    //     $subject = $_POST['theme']; //тема
-    //     $message = "Имя: ".$_POST['name']."\r\n";
-    //     $message .= "Телефон: ".$_POST['phone']."\r\n";
-    //     $message .= "Email: ".$_POST['Email'];
-    //     $headers = "From: $from\r\nReplay-To: $from\r\nContent-type: text/plain; charset=utf-8\r\n";
-    //     $result = mail($to, $subject, $message, $headers);
-    //
-    //     if ($result){
-    //         echo "Cообщение успешно отправленно. Пожалуйста, оставайтесь на связи";
-    //     }
-    // }
+        if ($result){
+            echo "Cообщение успешно отправленно. Пожалуйста, оставайтесь на связи";
+        }
+    }
 ?>
